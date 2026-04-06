dc3fa78df900ed24137db6dbb5078c245dbec986
BrockMorgan88 <brockjamesmorgan@gmail.com>
2026-04-06 13:20:03 +1000

    Enabled redistributable firmware - sounds useful, not sure why its disabled by default

--- NixOSConfig/other.nix
+++ NixOSConfig/other.nix
@@ -75,6 +75,8 @@
     ];
   };
 
+  hardware.enableRedistributableFirmware = true;
+
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11";
 }
