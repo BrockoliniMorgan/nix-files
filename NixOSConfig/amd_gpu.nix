a2ac70e1830b764cc147b83e43ca5dc4c22259d5
BrockMorgan88 <brockjamesmorgan@gmail.com>
2026-05-17 20:02:16 +1000

    Fixed btop not showing gpu

--- NixOSConfig/amd_gpu.nix
+++ NixOSConfig/amd_gpu.nix
@@ -1,5 +1,7 @@
 { lib, config, ... }:
 lib.mkIf config.has_amd_gpu {
+  nixpkgs.config.rocmSupport = true;
+  services.lact.enable = true;
   hardware = {
     amdgpu = {
       opencl.enable = true;
