f349c9f038ee2436ca88102198e241d788a7f615
BrockoliniMorgan <brockjamesmorgan@gmail.com>
2026-06-15 21:37:25 +1000

    Flake update

--- NixOSConfig/services.nix
+++ NixOSConfig/services.nix
@@ -18,11 +18,7 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
+      jack.enable = true;
     };
 
     # Enable touchpad support (enabled default in most desktopManager).
@@ -41,7 +37,7 @@
       };
     };
     # Enable the OpenSSH daemon.
-    # openssh.enable = true;
+    openssh.enable = true;
 
     udev = {
       enable = true;
